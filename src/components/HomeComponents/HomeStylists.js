f4e3db29741ae52a133465d1064ab068999d9455
cg-winder <chloewinder@hotmail.com>
2023-09-19 15:22:39 +0100

    buttons home resized

--- src/components/HomeComponents/HomeStylists.js
+++ src/components/HomeComponents/HomeStylists.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import team from "../imgs/utopiateam.jpeg"
+import mug from "../imgs/salonmug.jpeg"
 
 const HomeStylists = () => {
   return (
     <div className="homestylists-wrap">
         <div className="homestylists-header">Home to some of the best stylists in Lancashire</div>
-        <div className="homestylists-subtext">Utopia Hair Co is home to experienced and knowledgable stylists with a true passion for their craft. Whether it be hair, nails, lashes or brows, your stylist will work with you on a personal level using their skill and creativity for the perfect finish.</div>
-        <button className="homestylist-book-button">FIND YOUR STYLIST</button>
-        <img className="homestylist-img" src={team}/>
+        <div className="homestylists-text-img">
+          <img className="homestylists-img" src={mug}/>
+            <div className="homestylists-subtext">Utopia Hair Co is home to experienced and knowledgable stylists with a true passion for their craft. Whether it be hair, nails, lashes or brows, your stylist will work with you on a personal level using their skill and creativity for the perfect finish every time.</div>
+          </div>
+          <a href="/team" className="homestylists-button"><span>FIND YOUR STYLIST </span></a>
     </div>
   )
 }
