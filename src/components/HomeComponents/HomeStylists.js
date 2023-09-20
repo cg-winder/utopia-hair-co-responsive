e5475b843e70327e61618beb569588fadc9e62b3
cg-winder <chloewinder@hotmail.com>
2023-09-20 15:44:53 +0100

    all HomeStylists

--- src/components/HomeComponents/HomeStylists.js
+++ src/components/HomeComponents/HomeStylists.js
@@ -8,7 +8,7 @@ const HomeStylists = () => {
         <div className="homestylists-text-img">
           <img className="homestylists-img" src={mug}/>
             <div className="homestylists-subtext">Utopia Hair Co is home to experienced and knowledgable stylists with a true passion for their craft. Whether it be hair, nails, lashes or brows, your stylist will work with you on a personal level using their skill and creativity for the perfect finish every time.</div>
-          </div>
+            </div>
           <a href="/team" className="homestylists-button"><span>FIND YOUR STYLIST </span></a>
     </div>
   )
