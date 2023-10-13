95a1fc05fd6ef42e6e4a9543fa675af06699c340
cg-winder <chloewinder@hotmail.com>
2023-10-13 16:39:04 +0100

    homeinstagram

--- src/components/HomeComponents/HomeInstagram.js
+++ src/components/HomeComponents/HomeInstagram.js
@@ -4,7 +4,7 @@ const HomeInstagram = () => {
   return (
     <div className="homeinstagram">
       <div className="homeinstagram-header">The perfect finish every time</div>
-      <div class="elfsight-app-dc2cd2bf-8615-4fcc-bb9c-f546b5b3fe0d"></div>
+      <div class="elfsight-app-e4d52648-7280-417c-bd58-555b4f9c5f73"></div>
     </div>
   )
 }
