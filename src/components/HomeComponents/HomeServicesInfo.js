8ab71e5c2e3bc75a68617a9a7d13849d9c757299
cg-winder <chloewinder@hotmail.com>
2023-11-20 15:24:49 +0000

    homeservices 768 above

--- src/components/HomeComponents/HomeServicesInfo.js
+++ src/components/HomeComponents/HomeServicesInfo.js
@@ -12,20 +12,20 @@ const HomeServicesInfo = () => {
   return (
     <div className="homeservices-info-wrap">
       <div className="homeservices-info">
-  <div className="homeservices-hair">
-    <img src={hair}/><div><HomeSpecialiseHair /></div>
-  </div>
-  <div className="homeservices-nails">
-    <img src={nails}/><div><HomeSpecialiseNails /></div>
-  </div>
-  <div className="homeservices-brows">
-    <img src={brows}/><div><HomeSpecialiseBrows /></div>
-  </div>
-  <div className="homeservices-lashes">
-    <img src={lashes}/><div><HomeSpecialiseLashes /></div>
-  </div>
-      </div>
-      <div className="homeservices-link"><a href="/services"><span>More Treatment Details</span></a></div>
+          <div className="homeservices-hair">
+            <img src={hair}/><div><HomeSpecialiseHair /></div>
+          </div>
+          <div className="homeservices-nails">
+            <img src={nails}/><div><HomeSpecialiseNails /></div>
+          </div>
+          <div className="homeservices-brows">
+            <img src={brows}/><div><HomeSpecialiseBrows /></div>
+          </div>
+          <div className="homeservices-lashes">
+            <img src={lashes}/><div><HomeSpecialiseLashes /></div>
+          </div>
+        </div>
+        <a  href="/services" className="homeservices-link"><span>More Treatment Details</span></a>
     </div>
   )
 }
