cfb0326eba22990a8835bafd50e9f17d917513be
cg-winder <chloewinder@hotmail.com>
2023-11-27 14:09:00 +0000

    small additions

--- src/components/ServicesComponents/ServicesBook.js
+++ src/components/ServicesComponents/ServicesBook.js
@@ -5,7 +5,7 @@ const ServicesBook = () => {
   return (
     <div className="services-book-wrap">
         <div className="services-book-text">
-            <p className="services-book-1">Book appointment</p>
+            <p className="services-book-1">Book an appointment</p>
             <p className="services-book-2">Make a reservation for <div className="services-book-3">the ultimate pampering</div></p>
             <button
         className="services-book-button"
