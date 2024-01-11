684a2f50eae737cff9f0bcc1be6876f012f56aa0
cg-winder <chloewinder@hotmail.com>
2024-01-11 11:58:37 +0000

    sizing for homeservices and home stylists to fit better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,10 +22,10 @@ const Footer = () => {
         &copy;{new Date().getFullYear()} Utopia Hair Co | All rights reserved
       </p>
       <p className="footer-credit">
-        <a href="https://www.flaticon.com/free-icons/hair" title="hair icons" className="footer-icon-link"> Icon created by Freepik - Flaticon | </a>
-        <a href="https://www.flaticon.com/free-icons/eyebrow-pencil" title="eyebrow pencil icons" className="footer-icon-link"> Icon created by Soni Sokell - Flaticon | </a>
-        <a href="https://www.flaticon.com/free-icons/nail-art" title="nail art icons" className="footer-icon-link"> Icon created by Mayor Icons - Flaticon | </a>
-        <a href="https://www.flaticon.com/free-icons/eyelash" title="eyelash icons" className="footer-icon-link"> Icon created by ShofiyyullohFaiz - Flaticon</a></p>
+        <a href="https://www.flaticon.com/free-icons/hair" title="hair icons" className="footer-icon-link"> Hair icon created by Freepik - Flaticon | </a>
+        <a href="https://www.flaticon.com/free-icons/eyebrow-pencil" title="eyebrow pencil icons" className="footer-icon-link"> Brow icon created by Soni Sokell - Flaticon | </a>
+        <a href="https://www.flaticon.com/free-icons/nail-art" title="nail art icons" className="footer-icon-link"> Nail icon created by Mayor Icons - Flaticon | </a>
+        <a href="https://www.flaticon.com/free-icons/eyelash" title="eyelash icons" className="footer-icon-link"> Lash icon created by ShofiyyullohFaiz - Flaticon</a></p>
     </div>
   );
 };
