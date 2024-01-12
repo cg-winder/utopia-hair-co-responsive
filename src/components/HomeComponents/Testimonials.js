bb4113be93c0b730a16720ef8fe991eff3746593
cg-winder <chloewinder@hotmail.com>
2024-01-12 13:35:46 +0000

    testimonials

--- src/components/HomeComponents/Testimonials.js
+++ src/components/HomeComponents/Testimonials.js
@@ -12,6 +12,10 @@ const Testimonials = () => {
   <input type="radio" name="slider" id="slide4" className="slider-nav"/>
   <input type="radio" name="slider" id="slide5" className="slider-nav"/>
   <input type="radio" name="slider" id="slide6" className="slider-nav"/>
+  <input type="radio" name="slider" id="slide7" className="slider-nav"/>
+  <input type="radio" name="slider" id="slide8" className="slider-nav"/>
+  <input type="radio" name="slider" id="slide9" className="slider-nav"/>
+  <input type="radio" name="slider" id="slide10" className="slider-nav"/>
   <div className="slider-inner">
     <div className="slider-contents">
       <h2 className="slider-caption">Sophie E.</h2>
@@ -32,17 +36,32 @@ const Testimonials = () => {
             The salon is stunning and I can’t wait for my next visit.</p>
     </div>
     <div className="slider-contents">
-      <h2 className="slider-caption">Kara G.</h2>
-      <p className="slider-text">Beautiful salon. Great advice on my hair.</p>
+      <h2 className="slider-caption">Chelsea E.</h2>
+      <p className="slider-text">My first visit with Ellie, she did exactly what I wanted and was very professional through out! My colours look fab and very vibrant, will be returning!</p>
     </div>
     <div className="slider-contents">
-      <h2 className="slider-caption">Michelle H.</h2>
-      <p className="slider-text">Always a great experience. I leave with healthy hair every
-            time.</p>
+      <h2 className="slider-caption">Gillian C.</h2>
+      <p className="slider-text">Nicola is a very talented professional, friendly and approachable owner/hairdresser and has a friendly team. The setting is beautiful and the team are in great demand, highlighting their own individual capabilities.</p>
+    </div>
+    <div className="slider-contents">
+      <h2 className="slider-caption">Chantelle.</h2>
+      <p className="slider-text">Erin is a lovely girl with lots of patience when I changed my mind about adding extra glitter to my nail design. Also great with LVL lashes and brows too.</p>
     </div>
     <div className="slider-contents">
       <h2 className="slider-caption">Abigail T.</h2>
       <p className="slider-text">Always a friendly and lovely atmosphere with Nicola and she knows what’s she’s doing when it comes to hair! Wouldn’t trust anyone else touching my hair.</p>
+    </div>
+    <div className="slider-contents">
+      <h2 className="slider-caption">Sarah R.</h2>
+      <p className="slider-text">My hair looks beautiful again. Gorgeous chilled salon, good chats with Nicola and great coffee and cake!</p>
+    </div>
+    <div className="slider-contents">
+      <h2 className="slider-caption">Alycia M.</h2>
+      <p className="slider-text">I have been going to Erin for a while now for my nails and she always goes above and beyond! Really takes her time to ensure that they’re as I want them. She is such a lovely girl and I wouldn’t go anywhere else.</p>
+    </div>
+    <div className="slider-contents">
+      <h2 className="slider-caption">Lesley T.</h2>
+      <p className="slider-text">Grateful for friendly and professional advice from Ellie following initial consultation. Very happy with both colour and cut. Lovely salon.</p>
     </div>
     
   </div>
